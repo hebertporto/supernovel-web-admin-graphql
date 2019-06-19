f450b897478f4a7e96e04b829669bc0b59aaa855
Hebert Porto <hebert.porto@skipthedishes.ca>
2019-06-18 22:59:15 -0500

    setup basic context user and auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { useUser } from './context/auth';
-import AuthenticatedApp from './authenticated-app';
-import UnauthenticatedApp from './unauthenticated-app';
+import { useUser } from './context/user-context';
+import { useAuth } from './context/auth-context';
+// import AuthenticatedApp from './authenticated-app';
+// import UnauthenticatedApp from './unauthenticated-app';
+
+// const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
+// const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'));
+
+function Logado() {
+  const { logout } = useAuth();
+  return (
+    <div>
+      <p>*@ LOGADO @*</p>
+      <button onClick={logout}>LOGOUT</button>
+    </div>
+  );
+}
+
+function NaoLogado() {
+  const { login } = useAuth();
+  return (
+    <div>
+      <p>### NÃO LOGADO ###</p>
+      <button onClick={login}>LOGIN</button>
+    </div>
+  );
+}
 
 function App() {
   const user = useUser();
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  console.log('user: ', user);
+  return user ? <Logado /> : <NaoLogado />;
 }
 
 export { App };
---
+++ src/context/auth-context.js
@@ -0,0 +1,33 @@
+import React, { useState, createContext, useContext } from 'react';
+
+const AuthContext = createContext();
+
+function AuthProvider(props) {
+  const [user, setUser] = useState(false);
+
+  const login = () =>
+    setUser({
+      name: 'Hebert Porto',
+      email: 'hebertporto@gmail.com',
+      token: 'Ads@XWasdW%@2a#2apo&',
+    });
+  const logout = () => setUser(false);
+  const register = () => login();
+
+  return (
+    <AuthContext.Provider
+      value={{ user, login, logout, register }}
+      {...props}
+    />
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error(`useAuth must be used within a AuthProvider`);
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
